0548fbcdd095654d07b513ec6b74b923c8a049ac
Mo Elazab <68790124+melazab@users.noreply.github.com>
2022-08-26 15:58:27 -0400

    Minor update to get_userinput
    User no longer needs to input concentration.

--- get_userinput.m
+++ get_userinput.m
@@ -1,6 +1,18 @@
-dur = input("Duration between volume fills (in seconds): ");
-fill =input ("Volume of each bolus fill (in mL): ");
-v_f = input("Total volume infused (in mL):" );
-v_i = 0; % initial volume 
+dur = 40; %input("Duration between volume fills (in seconds): ");
+fill =60; %input ("Volume of each bolus fill (in mL): ");
+v_f = 420; % input("Total volume infused (in mL):" );
 n_fills = v_f/fill + 1; 
-volume = linspace(v_i,v_f, n_fills)
+volume = linspace(0, v_f, n_fills);
+
+
+if trial==1
+    conc = get_concentration(fileName);
+else
+    for i=1:trial
+        conc{i,:} = get_concentration(fileName{i});
+    end
+end
+
+
+
+
