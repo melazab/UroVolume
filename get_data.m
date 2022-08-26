53af635ed52a0713cb559236cbfb743c7c9ddf7a
Mo Elazab <68790124+melazab@users.noreply.github.com>
2022-08-26 15:59:25 -0400

    Minor update to get_data
    Added header including acceptable fileName naming convention

--- get_data.m
+++ get_data.m
@@ -1,5 +1,28 @@
-% This program reads signal express  data (usually saved as text files)
-clear;clc;close all
-[fileName1, pathName1] = uigetfile('*.txt');
-G = load(fullfile(pathName1,fileName1));
+% This program reads signal express data (usually saved as text files)
+% a typical file should have the following naming convention
+% Sensor-concentrationNaClsoln-medium-Rsensevalue-trial#.txt
+% An example of a file is 8.A-2.0NaCl-balloon-1kOhm-trial1.txt
 
+%%  Use file explorer to select file(s) containing conductance data
+clear;clc;
+[fileName, pathName] = uigetfile('*.txt','Select conductance files',...
+    'MultiSelect','on');
+
+
+% %% Manually inputing file path and name
+% pathName = "C:\Users\moudy\OneDrive - Cleveland State University..." + ...
+%     "\Lab\Uromonitor\SignalExpress Data\08-17-2022\1Vsin(100Hz)\";
+% fileName = "8.A-0.5NaCl-balloon-1kOhm-trial1.txt";
+
+%% Importing Conductance into MATLAB workspace
+if isa(fileName,'cell')
+    trial = length(fileName);
+    for i = 1:length(fileName)
+        G{:,i}= load(fullfile(pathName,char(fileName(1,i))));
+    end    
+elseif isa(fileName,'char')
+    trial = 1;
+    G{1} = load(fullfile(pathName,fileName));
+else
+    error('You did not select any files!')
+end
